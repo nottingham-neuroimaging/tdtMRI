3eca08549ebf710006318e271f7e007705c41ae9
julienbesle <julienbesle@gmail.com>
2022-05-19 13:47:00 +0300

    oneBackTask.m (renamed from test.m): parametrized probabiltiy of repeated stimulus

--- Visual stimulation/experiments/oneBackTask.m
+++ Visual stimulation/experiments/oneBackTask.m
@@ -1,4 +1,4 @@
-function [params,stimulus] = test(params,TR)
+function [params,stimulus] = oneBackTask(params,TR)
 
 % stimulus = struct array with fields:    
     %condition (integer, 0 = baseline)
@@ -16,14 +16,17 @@ end
 if fieldIsNotDefined(params,'nBlockTypes')
   params.nBlockTypes = 2;
 end
-if fieldIsNotDefined(params,'blockDurationS')
-  params.blockDurationS = 8;
-end
 if fieldIsNotDefined(params,'nBlockRepeats')
   params.nBlockRepeats = 3;
 end
+if fieldIsNotDefined(params,'blockDurationS')
+  params.blockDurationS = 30;
+end
 if fieldIsNotDefined(params,'stimPerBlock')
-  params.stimPerBlock = 8;
+  params.stimPerBlock = 20;
+end
+if fieldIsNotDefined(params,'repeatPeriod')
+  params.repeatPeriod = 6; % average number of stimuli between target repeated stimuli
 end
 if fieldIsNotDefined(params,'ISI')
   params.ISI = .2;
@@ -31,7 +34,6 @@ end
 if fieldIsNotDefined(params,'widthDeg') % stimulus width in degrees of visual angle
   params.widthDeg = 20;
 end
-
 
 if nargout==1
   return;
@@ -52,6 +54,7 @@ end
 cStim = 0;
 for iBlock = blocks
   if ~iBlock % Baseline
+    
     cStim = cStim+1;
     stimulus(cStim).conditionName = 'Baseline';
     stimulus(cStim).condition = 0;
@@ -59,11 +62,30 @@ for iBlock = blocks
     stimulus(cStim).duration= params.blockDurationS;
     stimulus(cStim).widthDeg = [];
     stimulus(cStim).centreDeg = [];
+    
   else
+    
+    repeatPreviousStim = false;
+    whichStim = randperm(length(stimFilenames{iBlock}),1); %choose first stimulus at random
+    previousStim = whichStim;
     for iStim  = 1:params.stimPerBlock
-      % randomly choose a stimulus in this block type
       cStim = cStim+1;
-      whichStim = randperm(length(stimFilenames{iBlock}),1); 
+      if repeatPreviousStim % repeated stimulus (1-back task target)
+        whichStim = previousStim;
+        repeatPreviousStim = false; % never repeat twice in a row
+      else
+        % randomly choose a stimulus in this block type (without repetition)
+        while whichStim == previousStim
+          whichStim = randperm(length(stimFilenames{iBlock}),1);
+        end
+        % decide whether to repeat this stimulus next
+        if iStim > 1 % (never repeat the first stimulus)
+          repeatPreviousStim =  randperm(params.repeatPeriod,1) == params.repeatPeriod;
+        end
+      end
+      previousStim = whichStim;
+      
+      % stimulus properties
       stimulus(cStim).conditionName = blockTypes{iBlock};
       stimulus(cStim).condition = iBlock;
       stimulus(cStim).filename = stimFilenames{iBlock}{whichStim};
@@ -79,6 +101,7 @@ for iBlock = blocks
       stimulus(cStim).widthDeg = [];
       stimulus(cStim).centreDeg = [];
     end
+    
   end
 end
 
